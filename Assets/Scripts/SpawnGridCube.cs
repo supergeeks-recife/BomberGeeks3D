a770fc4a4a6d7711854dc4359a4f0c5c26270096
kleberkgns <kleber.silva.kgns@gmail.com>
2023-03-29 18:00:49 -0300

    SpawnGridCube complete

--- Assets/Scripts/SpawnGridCube.cs
+++ Assets/Scripts/SpawnGridCube.cs
@@ -7,12 +7,31 @@ public class SpawnGridCube : MonoBehaviour
     [Header("Configurations")]
     public int gridX;
     public int gridZ;
+    public float gridSpacingOffset = 1f;
+    [SerializeField] Vector3 gridOrigin = Vector3.zero;
 
     [Header("Floor")]
     [SerializeField] GameObject[] blockFloorToPickFrom;
 
     [Header("Environments")]
     [SerializeField] GameObject[] environmentsToPickFrom;
+
+    void Start()
+    {
+        SpawnGridFloor();
+    }
+
+    void SpawnGridFloor()
+    {
+        for (int x = 0; x < gridX; x++)
+        {
+            for (int z = 0; z < gridZ; z++)
+            {
+                Vector3 spawnPositionFloor = new Vector3(x, 0, z) + gridOrigin;
+                PickAndSpawnFloor(spawnPositionFloor, Quaternion.identity);
+            }
+        }
+    }
 
     void PickAndSpawnFloor(Vector3 positionToSpawn, Quaternion rotationToSpawn)
     {
