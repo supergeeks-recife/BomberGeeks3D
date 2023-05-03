debff98c8a1c4d93418aa7996367059dd5c74f87
kleberkgns <kleber.silva.kgns@gmail.com>
2023-05-03 17:49:45 -0300

    lobbyManager script

---
+++ Assets/Scripts/Network/LobbyManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LobbyManager : MonoBehaviour
+{
+    [SerializeField] GameObject panelLogin;
+    [SerializeField] GameObject panelLobby;
+
+    public TMP_Text lobbyStartTime;
+    public TMP_InputField playerNameInputField;
+    public string playerName;
+    public TMP_Text connectionStatusText;
+    void Start()
+    {
+        lobbyStartTime.gameObject.SetActive(false); 
+        connectionStatusText.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void PanelLobbyActive()
+    {
+        panelLobby.SetActive(true);
+        panelLogin.SetActive(false);
+    }
+
+    public void PanelLoginActive()
+    {
+        panelLobby.SetActive(false);
+        panelLogin.SetActive(true);
+    }
+}
